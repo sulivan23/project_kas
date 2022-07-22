c0f675309e5ffc1a7542c0b1455030a461b2247d
AndhyAndhika <dhikbang@gmail.com>
2022-07-22 15:08:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                                        <!-- Latest compiled and minified CSS -->
+    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet">
-                                               <!-- CSS LOCAL -->
+    <!-- CSS LOCAL -->
     <link rel="stylesheet" href="assets/style.css">
 
     <title>Kas-TF3C - Login</title>
@@ -18,27 +18,30 @@
 
 
 <body class="bg-white">
-<div class="container">
-<h3 class="text-center mt-5 text-white">Selamat Datang Di Management kas TF4C</h3>
-    <div class="row">
-        <div class="col-md-6 justify-content-center">
-            <div class="card login shadow">
-                <div class="card-header login-header bg-primary text-white text-center"><h5 class="text-bold">Login Project Kas</h5></div>
-                <div class="card-body login-body text-dark">
-                    <form action="proses/proses_login.php" method="POST">
-                        <label>Username</label>
-                        <br>
-                        <input type="text" class="form-control" placeholder="Masukan user... "name="username">
-                        <br>
-                        <label>Password</label>
-                        <br>
-                        <input type="password" class="form-control" placeholder="Masukan pass..."name="password">
-                        <button class="btn btn-primary text-white mt-4 w-100">Login</button>
-                    </form>
+    <div class="container">
+        <h3 class="text-center mt-5 text-white">Selamat Datang Di Management kas TF4C</h3>
+        <div class="row">
+            <div class="col-md-6 justify-content-center">
+                <div class="card login shadow">
+                    <div class="card-header login-header bg-primary text-white text-center">
+                        <h5 class="text-bold">Login Project Kas</h5>
+                    </div>
+                    <div class="card-body login-body text-dark">
+                        <form action="proses/proses_login.php" method="POST">
+                            <label>Username</label>
+                            <br>
+                            <input type="text" class="form-control" placeholder="Masukan user... " name="username">
+                            <br>
+                            <label>Password</label>
+                            <br>
+                            <input type="password" class="form-control" placeholder="Masukan pass..." name="password">
+                            <button class="btn btn-primary text-white mt-4 w-100">Login</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </body>
+
 </html>
